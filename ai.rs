49533e4cdfcbfad5aad12b917a47fe1300900f8f
Viktor Ogeman <vron@users.noreply.github.com>
2023-03-03 20:48:22 +0100

    Update ai.rs

--- ai.rs
+++ ai.rs
@@ -5,8 +5,17 @@ pub struct Ai();
 
 impl rai::Ai for Ai {
     fn run<'a>(&self, e: Box<dyn rai::AiEnv + 'a>) -> rai::Action {
-        let r: u8 = 3;
-        return match r % 9 {
+        if e.no_resources(0.0, 0.0) > 0.0 {
+            return Create;
+        }
+
+        if e.no_enemies(0.0, 0.0) > 0.0 && e.no_enemies(0.0, 0.0) < e.no_friends(0.0, 0.0) {
+            return Nothing;
+        }
+        
+        let r = self.rnd(e);
+        let r: u8 = (r*8.0) as u8;
+        return match r % 8 {
             0 => MoveN,
             1 => MoveNE,
             2 => MoveE,
@@ -19,4 +28,15 @@ impl rai::Ai for Ai {
             _ => Nothing,
         };
     }
+
+    impl Ai {
+        fn rnd<'a>(&self, mut e: Box<dyn rai::AiEnv + 'a>) -> f32 {
+            let s1 = e.get_memory(0.0);
+            let s2 = s1 + 11.34;
+            let s3 = s2.rem_euclid(1.0);
+            e.set_memory(0.0, s3);
+            s3
+        }
+    }
+
 }
