abaf9681794511591dc864ec0575965255ae59c4
Viktor Ogeman <vron@users.noreply.github.com>
2023-03-04 07:34:52 +0100

    Update ai.rs

--- ai.rs
+++ ai.rs
@@ -24,7 +24,7 @@ impl rai::Ai for Ai {
             (-1.0, 1.0, MoveNW)
         ];
         for (dx, dy, a) in cases.into_iter() {
-            if e.no_resources(dx, dy) > 0.0 {
+            if e.no_resources(dx, dy) > 0.0 && e.no_enemies(dx, dy) < 1.0 && e.no_friends(dx, dy) < 1.0 && self.rnd(e) < 0.5 {
                 return a;
             }   
         }
